afbec8135d13487e47a001e2d9bcd28879efec73
lucapicovici <lucapicovici79@gmail.com>
2023-01-27 18:35:44 +0200

    Signup form; handling email and password inputs

---
+++ ticketing/client/pages/auth/signup.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(email, password);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};
+
+export default Signup;
