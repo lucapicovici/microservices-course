18e335b8260599b5ee220c041eae55bc5a8ef070
lucapicovici <lucapicovici79@gmail.com>
2023-01-28 15:35:06 +0200

    Overview on server side rendering

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,5 +1,13 @@
-const LandingPage = () => {
+const LandingPage = ({ color }) => {
+  console.log('I am in the component!', color);
   return <h1>Landing Page</h1>;
+};
+
+// Function to be executed during server side rendering process
+LandingPage.getInitialProps = () => {
+  console.log('I am on the server!');
+
+  return { color: 'red' };
 };
 
 export default LandingPage;
