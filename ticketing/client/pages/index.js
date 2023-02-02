2717b66956af1d2fefd495e2d4a22ae2e2af3a7d
lucapicovici <lucapicovici79@gmail.com>
2023-02-02 20:02:04 +0200

    A reusable API client

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   // console.log(currentUser);
@@ -11,28 +12,11 @@ const LandingPage = ({ currentUser }) => {
 };
 
 // Function to be executed during server side rendering process
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    // We are on the server!
-    // Requests should be made to http://ingress-nginx.ingress-nginx...
-    // http://SERVICENAME.NAMESPACE.svc.cluster.local
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers,
-      }
-    );
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
 
-    return data;
-  } else {
-    // We are on the browser!
-    // Requests can be made with a base url of ''
-    const { data } = await axios.get('/api/users/currentuser');
-
-    return data;
-  }
-
-  return {};
+  return data;
 };
 
 export default LandingPage;
