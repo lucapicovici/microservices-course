42e16037ed58dd65bc5a623565845e728e08f174
lucapicovici <lucapicovici79@gmail.com>
2023-02-02 14:46:22 +0200

    Specifying the host

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,18 +1,40 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser').catch((err) => {
+  //   console.log(err.message);
+  // });
   console.log(currentUser);
-  axios.get('/api/users/currentuser').catch((err) => {
-    console.log(err.message);
-  });
+
   return <h1>Landing Page</h1>;
 };
 
 // Function to be executed during server side rendering process
-// LandingPage.getInitialProps = async () => {
-//   const response = await axios.get('/api/users/currentuser');
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    // We are on the server!
+    // Requests should be made to http://ingress-nginx.ingress-nginx...
+    // http://SERVICENAME.NAMESPACE.svc.cluster.local
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev',
+        },
+      }
+    );
 
-//   return response.data;
-// };
+    return data;
+  } else {
+    // We are on the browser!
+    // Requests can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
+  }
+
+  return {};
+};
 
 export default LandingPage;
