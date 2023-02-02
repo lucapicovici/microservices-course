e59650d827fcc382d2d1f9a3f30a41ebeedd09ec
lucapicovici <lucapicovici79@gmail.com>
2023-02-02 14:53:59 +0200

    Passing through the cookies

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -11,7 +11,7 @@ const LandingPage = ({ currentUser }) => {
 };
 
 // Function to be executed during server side rendering process
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
   if (typeof window === 'undefined') {
     // We are on the server!
     // Requests should be made to http://ingress-nginx.ingress-nginx...
@@ -19,9 +19,7 @@ LandingPage.getInitialProps = async () => {
     const { data } = await axios.get(
       'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
       {
-        headers: {
-          Host: 'ticketing.dev',
-        },
+        headers: req.headers,
       }
     );
 
