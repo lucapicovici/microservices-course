024133503ccfd07453385afd19ac758bf112d3a7
lucapicovici <lucapicovici79@gmail.com>
2023-02-02 20:12:38 +0200

    Content on the landing page

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -2,13 +2,11 @@ import axios from 'axios';
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  // console.log(currentUser);
-  // axios.get('/api/users/currentuser').catch((err) => {
-  //   console.log(err.message);
-  // });
-  console.log(currentUser);
-
-  return <h1>Landing Page</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
 };
 
 // Function to be executed during server side rendering process
