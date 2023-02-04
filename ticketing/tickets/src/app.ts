3a88709ac1b09e2c958d2761c7330c1c3c354822
lucapicovici <lucapicovici79@gmail.com>
2023-02-04 22:25:46 +0200

    Creating the router

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -2,8 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-
 import { errorHandler, NotFoundError } from '@iceydc-tickets/common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +14,8 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
