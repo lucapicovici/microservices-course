106c9ad011da895c6e164880fb12cbca776b189a
lucapicovici <lucapicovici79@gmail.com>
2023-02-24 21:09:27 +0200

    Singleton implementation (nats)

--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { natsWrapper } from './nats-wrapper';
 
 import { app } from './app';
 
@@ -11,6 +12,7 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect('ticketing', 'asdf1234', 'http://nats-srv:4222');
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (err) {
