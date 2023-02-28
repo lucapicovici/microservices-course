48416ab77e30b2489e3320c07b8799928765c333
lucapicovici <lucapicovici79@gmail.com>
2023-02-28 19:25:13 +0200

    Convenience document methods

--- ticketing/orders/src/models/order.ts
+++ ticketing/orders/src/models/order.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 import { OrderStatus } from '@iceydc-tickets/common';
 import { TicketDoc } from '../models/ticket';
 
+export { OrderStatus };
+
 interface OrderAttrs {
   userId: string;
   status: OrderStatus;
