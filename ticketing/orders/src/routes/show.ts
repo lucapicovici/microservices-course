d76ec38ded954b76d90e3bb7175407b2b217d77e
lucapicovici <lucapicovici79@gmail.com>
2023-03-02 20:25:39 +0200

    Fetching individual orders

--- ticketing/orders/src/routes/show.ts
+++ ticketing/orders/src/routes/show.ts
@@ -1,9 +1,29 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from '@iceydc-tickets/common';
 import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    // Optional check for valid ObjectId can be implemented later
+    const order = await Order.findById(req.params.orderId).populate('ticket');
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
