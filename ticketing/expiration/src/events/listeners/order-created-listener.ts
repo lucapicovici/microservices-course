0b076cc4fc6674f8705dabb3f97821a27a5e1414
lucapicovici <lucapicovici79@gmail.com>
2023-03-08 21:16:52 +0200

    Publishing an event on job processing

--- ticketing/expiration/src/events/listeners/order-created-listener.ts
+++ ticketing/expiration/src/events/listeners/order-created-listener.ts
@@ -3,10 +3,25 @@ import { Listener } from '@iceydc-tickets/common';
 import { OrderCreatedEvent } from '@iceydc-tickets/common/build/events/order-created-event';
 import { Subjects } from '@iceydc-tickets/common/build/events/subjects';
 import { queueGroupName } from './queue-group-name';
+import { expirationQueue } from '../../queues/expiration-queue';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log('Waiting this many miliseconds to process the job:', delay);
+
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay,
+      }
+    );
+
+    msg.ack();
+  }
 }
