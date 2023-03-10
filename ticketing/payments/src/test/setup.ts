38fc295e11461dbcfca29e6038d35083541092ec
lucapicovici <lucapicovici79@gmail.com>
2023-03-10 22:53:23 +0200

    Payments route handler testing

--- ticketing/payments/src/test/setup.ts
+++ ticketing/payments/src/test/setup.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
 
 declare global {
-  var signin: () => string[];
+  var signin: (id?: string) => string[];
 }
 
 jest.mock('../nats-wrapper');
@@ -34,11 +34,11 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = () => {
+global.signin = (id?: string) => {
   // Faking a JWT because there's no signup route on this service!
   // Build a JWT payload { id, email }
   const payload = {
-    id: new mongoose.Types.ObjectId().toHexString(),
+    id: id || new mongoose.Types.ObjectId().toHexString(),
     email: 'test@test.com',
   };
 
