b48211f683f354d639f3fd4f109cd407b8aae9c7
lucapicovici <lucapicovici79@gmail.com>
2023-03-11 22:12:12 +0200

    Mocked stripe client

--- ticketing/payments/src/routes/new.ts
+++ ticketing/payments/src/routes/new.ts
@@ -7,6 +7,7 @@ import {
   requireAuth,
   validateRequest,
 } from '@iceydc-tickets/common';
+import { stripe } from '../stripe';
 import { body } from 'express-validator';
 import { Order } from '../models/order';
 
@@ -32,7 +33,13 @@ router.post(
       throw new BadRequestError('Cannot pay for a cancelled order');
     }
 
-    res.send({ success: true });
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100,
+      source: token,
+    });
+
+    res.status(201).send({ success: true });
   }
 );
 
