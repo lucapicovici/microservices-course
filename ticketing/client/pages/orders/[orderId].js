ce056f91b5846e1815cf489eed2fa29ffb0cca4a
lucapicovici <lucapicovici79@gmail.com>
2023-03-14 20:20:40 +0200

    Rendering a list of orders; section 22

--- ticketing/client/pages/orders/[orderId].js
+++ ticketing/client/pages/orders/[orderId].js
@@ -1,9 +1,19 @@
+import Router from 'next/router';
 import { useEffect, useState, useRef } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import useRequest from '../../hooks/use-request';
 
 const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState('');
   const intervalRef = useRef(null);
+  const { doRequest, errors } = useRequest({
+    url: '/api/payments',
+    method: 'post',
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: () => Router.push('/orders'),
+  });
 
   useEffect(() => {
     const findTimeLeft = () => {
@@ -28,11 +38,12 @@ const OrderShow = ({ order, currentUser }) => {
     <div>
       Time left to pay: {timeLeft} seconds
       <StripeCheckout
-        token={(token) => console.log(token)}
+        token={({ id }) => doRequest({ token: id })}
         stripeKey="pk_test_51GyhSUBVEY9pRgXUOkf0AsEicjgkjFr9znVhSysqTBEFqy6lMGuORtv3FV2yhbH96PPuuj8XKj0hoCQ6tKzBWimv00S9nDk8PF"
         amount={order.ticket.price * 100}
         email={currentUser.email}
       />
+      {errors}
     </div>
   );
 };
